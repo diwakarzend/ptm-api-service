d7733d1ad3ae0c83e53b6c58340bde323f4ab2ff
aman.garg <aman.garg@zee.com>
2022-05-24 10:15:07 +0530

    DB structure and build change

--- src/main/resources/sql/changeQuery23052022.sql
+++ src/main/resources/sql/changeQuery23052022.sql
@@ -3,6 +3,7 @@ CREATE TABLE `ptm_api_service`.`ptm_ptp_detail` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_uuid` VARCHAR(100) NULL,
   `vendor_id` VARCHAR(100) NULL,
+  `qr_details` VARCHAR(100) NULL,
   `total_limit` INT NOT NULL,
   `daily_limit` INT NOT NULL,
   `running_date` DATETIME NULL,
